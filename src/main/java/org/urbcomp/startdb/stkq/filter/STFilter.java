19e1476719dd8291064e7dace1381680733bc041
Leo <liruiyuanwhu@qq.com>
2023-12-25 20:54:28 +0800

    refactor

--- src/main/java/org/urbcomp/startdb/stkq/filter/STFilter.java
+++ src/main/java/org/urbcomp/startdb/stkq/filter/STFilter.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class STFilter extends AbstractSTFilter implements ISTKFilter {
+public class STFilter extends AbstractSTFilter {
     private final IFilterManager filterManager;
 
     public STFilter(int sBits, int tBits, IFilterManager filterManager) {
